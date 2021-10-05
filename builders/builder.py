96675ea8b24df9ad3806d5ceb6845512b8dd8b2d
Renzo Gambone <renzodgc12@gmail.com>
2021-10-05 12:51:21 -0300

    Updated to Jetpack 4.6.1

--- builders/builder.py
+++ builders/builder.py
@@ -3,7 +3,7 @@ def build_detection_model(name, config):
         from detectors.x86_detector import X86Detector
         image_width = config["image_width"]
         image_height = config["image_height"]
-        thresh = config["threshold"]
+        image_thresh = config["threshold"]
         detector = X86Detector(width=image_width, height=image_height, thresh=image_thresh)
     else:
         raise ValueError('Not supported detector named: ', name, ' for AlphaPose.')
